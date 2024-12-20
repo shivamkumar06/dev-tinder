a61f3aec8623e5b92a1bf798d649cc428851656d
Shivam Kumar <shivam.saha06@gmail.com>
2024-12-20 23:17:26 +0530

    exploring with GET, POST, and DELETE methods

--- src/app.js
+++ src/app.js
@@ -1,16 +1,29 @@
 const express = require("express");
 const app = express();
 const port = 7777;
-app.use("/", (req, res) => {
-  res.send("Welcome to Express");
-});
 
 app.use("/test", (req, res) => {
   res.send("test page");
 });
-app.use("/hello", (req, res) => {
-  res.send("Hello World");
+
+// app.use("/users", (req, res) => {
+//   res.send("HAHAHAHAHA");
+// });
+
+app.get("/users", (req, res) => {
+  res.send({ firstName: "Shivam", lastName: "Kumar" });
 });
+
+app.post("/users", (req, res) => {
+  console.log("POST request");
+  res.send({ message: "Post recived sucessfully" });
+});
+
+app.delete("/users", (req, res) => {
+  console.log("DELETE request received");
+  res.send({ message: "Deleted sucessfully" });
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
